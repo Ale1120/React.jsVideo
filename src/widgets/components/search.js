78d1ca53e665704a32861a8ccdd433096cb96526
alejandro <alejandro@ezgmz.com>
2018-12-03 21:26:36 -0400

    adds search

---
+++ src/widgets/components/search.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './search.css';
+
+const Search = () => (
+	<form className="Search">
+		<input 
+		type="text"
+		placeholder="Busca Tu video Favorito" 
+		className="Search-input"
+		/>
+	</form>
+)
+
+export default Search
---
+++ src/widgets/containers/search.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import Search from '../components/search';
+
+class SearchContainer extends Component {
+	render() {
+		return (
+			<Search />
+			)
+	}
+}
+
+
+export default SearchContainer;
