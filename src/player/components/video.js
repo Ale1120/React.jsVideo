0db1847185ab43cfa69fbfbc8a445c6dc421da2e
alejandro <alejandro@ezgmz.com>
2018-12-04 19:54:08 -0400

    add title and video font

---
+++ src/player/components/video.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import  './video.css'
+class Video extends Component {
+	render() {
+		return (
+		  <div className='Video'>		
+		  	<video
+          	  autoPlay={this.props.autoplay}
+          	  src={this.props.src}
+            />
+          </div>
+		)
+	}
+}
+
+export default Video;
